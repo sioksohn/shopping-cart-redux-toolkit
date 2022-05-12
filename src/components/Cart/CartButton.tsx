62607d3b15c11fc123701d1be4328ed5fd10556f
Elaine <sosohn@gmail.com>
2022-05-12 13:41:12 -0700

    Add store with ui-slice with Redux hooks

--- src/components/Cart/CartButton.tsx
+++ src/components/Cart/CartButton.tsx
@@ -1,8 +1,16 @@
 import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useAppDispatch } from "../../app/hooks";
 
 const CartButton = () => {
+  const dispatch = useAppDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
